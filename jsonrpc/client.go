370c8ea31e1800346f8ff79a19f5854ee6083abb
chenxiaojing <chxj1992@gmail.com>
2016-02-16 15:26:36 +0800

    save

--- jsonrpc/client.go
+++ jsonrpc/client.go
@@ -11,26 +11,32 @@ type Client struct {
 	Url string
 }
 
-func (p Client) Call(method string, args interface{}, result interface{}) {
+func (p Client) Call(method string, args interface{}, result interface{}) error {
 
 	message, err := json2.EncodeClientRequest(method, args)
 	if err != nil {
-		log.Fatalf("%s", err)
+		log.Printf("%s", err)
+		return err
 	}
 	req, err := http.NewRequest("POST", p.Url, bytes.NewBuffer(message))
 	if err != nil {
-		log.Fatalf("%s", err)
+		log.Printf("%s", err)
+		return err
 	}
 	req.Header.Set("Content-Type", "application/json")
 	client := new(http.Client)
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Error in sending request to %s. %s", p.Url, err)
+		log.Printf("Error in sending request to %s. %s", p.Url, err)
+		return err
 	}
 	defer resp.Body.Close()
 
 	err = json2.DecodeClientResponse(resp.Body, result)
 	if err != nil {
-		log.Fatalf("Couldn't decode response. %s", err)
+		log.Printf("Couldn't decode response. %s", err)
+		return err
 	}
+
+	return err
 }
